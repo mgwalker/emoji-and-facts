0f2b5df8ace281ffa70022e130c7b82dfb2747b3
Greg Walker <mgwalker@gmail.com>
2019-10-19 21:57:22 -0500

    update emoji

--- all/getEmojiFromBrowser.js
+++ all/getEmojiFromBrowser.js
@@ -26,13 +26,44 @@
     return response.json();
   };
 
+  const blackout = document.createElement('div');
+  blackout.setAttribute(
+    'style',
+    'position: fixed; top: 0; left: 0; height: 100vh; width: 100vw; background-color: rgba(0, 0, 0, 0.3);'
+  );
+  document.body.appendChild(blackout);
+
+  const banner = document.createElement('div');
+  banner.setAttribute(
+    'style',
+    'position: fixed; top: 45vh; left: 30vw; width: 40vw; background: white; text-align: center; padding: 10vh;'
+  );
+  document.body.appendChild(banner);
+
+  banner.innerText = 'Getting page 1...';
   let page = await getPage();
   const emoji = page.emoji;
   while (page.paging.pages !== page.paging.page) {
-    await sleep(3000);
+    await sleep(1.5e3);
+    banner.innerText = `Getting page ${page.paging.page + 1} of ${
+      page.paging.pages
+    }...`;
     page = await getPage(page.paging.page + 1);
     emoji.push(...page.emoji);
   }
 
-  window.open(`data:application/json,${JSON.stringify(emoji, null, 2)}`);
+  banner.remove();
+  blackout.remove();
+
+  const anchor = document.createElement('a');
+  anchor.setAttribute('download', 'emoji.json');
+  anchor.setAttribute(
+    'href',
+    `data:application/json,${JSON.stringify(emoji, null, 2)}`
+  );
+  document.body.appendChild(anchor);
+  await sleep(10);
+  anchor.click();
+  await sleep(10);
+  anchor.remove();
 })();
