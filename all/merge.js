eab80c7f076c3f11c4a16c9631503a86d15a7cc4
Greg Walker <mgwalker@gmail.com>
2022-05-28 19:13:59 -0500

    one more update

--- all/merge.js
+++ all/merge.js
@@ -3,7 +3,7 @@ const https = require('https');
 const path = require('path');
 const readline = require('readline').createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 // Works best in iTerm 2.9 or higher, but whatever!
@@ -27,20 +27,20 @@ const draw = (() => {
     }
   }
 
-  return async url =>
-    new Promise(resolve => {
+  return async (url) =>
+    new Promise((resolve) => {
       const esc = '\u001B]1337;File=inline=1:';
       const bel = `\u0007`;
 
       let img = '';
-      https.get(url, res => {
+      https.get(url, (res) => {
         res.setEncoding('base64');
 
-        res.on('data', d => {
+        res.on('data', (d) => {
           img += d;
         });
 
-        res.on('end', a => {
+        res.on('end', (a) => {
           console.log(`${esc}${img}${bel}`);
           resolve();
         });
@@ -54,33 +54,33 @@ const ignore = require('./ignore.json');
 // Get the complete list of emoji, as presented
 const all = require('./emoji.json')
   // Add the eventual filename as a property
-  .map(e => ({ ...e, filename: `${e.name}${path.extname(e.url)}` }))
+  .map((e) => ({ ...e, filename: `${e.name}${path.extname(e.url)}` }))
   // Remove aliases
-  .filter(e => e.is_alias === 0);
+  .filter((e) => e.is_alias === 0);
 
 const newEmoji = all
   // Remove the emoji that we already know we don't want to publish
-  .filter(e => !ignore.includes(e.filename))
+  .filter((e) => !ignore.includes(e.filename))
   // Remove the emoji that are already part of the repo
-  .filter(e => !fs.existsSync(`../emoji/${e.filename}`));
+  .filter((e) => !fs.existsSync(`../emoji/${e.filename}`));
 
 // Prompt the user about whether to keep a particular emoji
-const shouldKeep = async emoji =>
-  new Promise(async resolve => {
+const shouldKeep = async (emoji) =>
+  new Promise(async (resolve) => {
     console.log(`Here's a new emoji called "${emoji.name}":`);
     await draw(emoji.url);
 
-    readline.question('Do you want to keep this one? [y/N] ', yn => {
+    readline.question('Do you want to keep this one? [y/N] ', (yn) => {
       resolve(/^y/i.test(yn));
     });
   });
 
 // Download an emoji
-const download = async emoji =>
-  new Promise(resolve => {
+const download = async (emoji) =>
+  new Promise((resolve) => {
     const file = fs.createWriteStream(`../emoji/${emoji.filename}`);
     https
-      .get(emoji.url, function(res) {
+      .get(emoji.url, function (res) {
         res.pipe(file);
         file.on('finish', () => {
           file.close();
@@ -88,7 +88,7 @@ const download = async emoji =>
           resolve();
         });
       })
-      .on('error', function(e) {
+      .on('error', function (e) {
         file.close();
         fs.unlink(`../emoji/${emoji.filename}`);
         console.log('download failed');
@@ -128,11 +128,15 @@ const promptAboutAllTheEmoji = async () => {
 // Build the flat list of emoji files, based on what's currently on the
 // filesystem. This list is used to power the simple landing page.
 const buildEmojiList = async () =>
-  new Promise(resolve => {
+  new Promise((resolve) => {
     fs.readdir('../emoji', (err, files) => {
       fs.writeFileSync(
         '../emoji.json',
-        JSON.stringify(files.map(f => `emoji/${f}`), null, '  ')
+        JSON.stringify(
+          files.map((f) => `emoji/${f}`),
+          null,
+          '  '
+        )
       );
       resolve();
     });
@@ -141,19 +145,19 @@ const buildEmojiList = async () =>
 // Build a bit more metadata. This one is used to build the grid page
 // and needs to include aliases and everything for tooltips.
 const buildEmojiMetadata = async () =>
-  new Promise(resolve => {
+  new Promise((resolve) => {
     fs.readdir('../emoji', (_, files) => {
       const meta = files.reduce((o, filename) => {
         // We found an emoji file that's not in the metadata list.
         // So we'll just make stuff up, based on the filename.
-        const emoji = all.find(e => e.filename === filename);
+        const emoji = all.find((e) => e.filename === filename);
         if (!emoji) {
           return {
             ...o,
             [path.basename(filename)]: {
               aliases: [path.basename(filename)],
-              file: `emoji/${filename}`
-            }
+              file: `emoji/${filename}`,
+            },
           };
         }
 
@@ -167,8 +171,8 @@ const buildEmojiMetadata = async () =>
             // aliases list will always include the base emoji, whether it
             // has other aliases or not.
             aliases: emoji.synonyms.length ? emoji.synonyms : [emoji.name],
-            file: `emoji/${filename}`
-          }
+            file: `emoji/${filename}`,
+          },
         };
       }, {});
       fs.writeFileSync('../meta.json', JSON.stringify(meta, null, 2));
@@ -184,7 +188,7 @@ promptAboutAllTheEmoji()
     await buildEmojiList();
     await buildEmojiMetadata();
   })
-  .catch(e => {
+  .catch((e) => {
     console.log(e);
     readline.close();
   });
--- upload/upload.js
+++ upload/upload.js
@@ -1,61 +1,122 @@
 const fs = require('fs');
 const https = require('https');
 const qs = require('querystring');
+const readline = require('readline');
 const request = require('request');
 
-const team = process.env.SLACK_TEAM;
-const token = process.env.SLACK_TOKEN;
+(async () => {
+  let team = process.env.SLACK_TEAM;
+  let token = process.env.SLACK_TOKEN;
 
-const cookie = require('./cookie');
+  const prompt = (() => {
+    const rl = readline.createInterface(process.stdin, process.stdout);
+    return (q) =>
+      new Promise((resolve) => {
+        rl.question(q, (a) => {
+          resolve(a);
+        });
+      });
+  })();
 
-const sleep = async (ms) =>
-  new Promise((resolve) => {
-    setTimeout(() => resolve(), ms);
+  if (!team) {
+    team = await prompt('Slack team? ');
+  }
+  if (!token) {
+    token = await prompt('Slack token? ');
+  }
+
+  const cookie = require('./cookie');
+
+  const sleep = async (ms) =>
+    new Promise((resolve) => {
+      setTimeout(() => resolve(), ms);
+    });
+
+  const emoji = require('../meta');
+  const skip = require('./skip.json');
+
+  const nameCleanupRegex = /\.[a-z]+$/i;
+  Object.entries(emoji).forEach(([name, meta]) => {
+    const clean = name.replace(nameCleanupRegex, '');
+    delete emoji[name];
+
+    if (!skip.includes(clean)) {
+      emoji[clean] = meta;
+
+      meta.aliases = meta.aliases.map((alias) =>
+        alias.replace(nameCleanupRegex, '')
+      );
+    }
   });
 
-const emoji = require('../meta');
-const skip = require('./skip.json');
+  const getEmoji = async () => {
+    const bodyData = {
+      token,
+    };
 
-const nameCleanupRegex = /\.[a-z]+$/i;
-Object.entries(emoji).forEach(([name, meta]) => {
-  const clean = name.replace(nameCleanupRegex, '');
-  delete emoji[name];
+    const getPage = async (page = 1) =>
+      new Promise((pageResolve) => {
+        bodyData.page = page;
+        let body = qs.stringify(bodyData);
+        const req = https.request(
+          {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'Content-Length': body.length,
+              Cookie: cookie,
+            },
+            host: `${team}.slack.com`,
+            method: 'POST',
+            path: '/api/emoji.adminList',
+            port: 443,
+          },
+          (res) => {
+            let out = '';
+            res.on('data', (d) => (out += d));
+            res.on('end', () => {
+              pageResolve(JSON.parse(out));
+            });
+          }
+        );
 
-  if (!skip.includes(clean)) {
-    emoji[clean] = meta;
+        req.write(body);
+        req.end();
+      });
 
-    meta.aliases = meta.aliases.map((alias) =>
-      alias.replace(nameCleanupRegex, '')
-    );
-  }
-});
+    let page = await getPage();
+    const emoji = page.emoji;
+    while (page.paging.pages !== page.paging.page) {
+      await sleep(1.5e3);
+      page = await getPage(page.paging.page + 1);
+      emoji.push(...page.emoji);
+    }
 
-const getEmoji = async () => {
-  const bodyData = {
-    token,
+    return emoji;
   };
 
-  const getPage = async (page = 1) =>
-    new Promise((pageResolve) => {
-      bodyData.page = page;
-      let body = qs.stringify(bodyData);
+  const putAlias = async (alias, source) =>
+    new Promise((resolve) => {
+      const body = qs.stringify({
+        alias_for: source,
+        mode: 'alias',
+        name: alias,
+        token,
+      });
+
       const req = https.request(
         {
           headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
             'Content-Length': body.length,
-            Cookie: cookie,
           },
           host: `${team}.slack.com`,
           method: 'POST',
-          path: '/api/emoji.adminList',
+          path: '/api/emoji.add',
           port: 443,
         },
         (res) => {
-          let out = '';
-          res.on('data', (d) => (out += d));
           res.on('end', () => {
-            pageResolve(JSON.parse(out));
+            resolve();
           });
         }
       );
@@ -64,105 +125,64 @@ const getEmoji = async () => {
       req.end();
     });
 
-  let page = await getPage();
-  const emoji = page.emoji;
-  while (page.paging.pages !== page.paging.page) {
-    await sleep(1.5e3);
-    page = await getPage(page.paging.page + 1);
-    emoji.push(...page.emoji);
-  }
-
-  return emoji;
-};
-
-const putAlias = async (alias, source) =>
-  new Promise((resolve) => {
-    const body = qs.stringify({
-      alias_for: source,
-      mode: 'alias',
-      name: alias,
-      token,
-    });
-
-    const req = https.request(
-      {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Content-Length': body.length,
+  const putEmoji = async (name, file) =>
+    new Promise((resolve) => {
+      const image = fs.createReadStream(`../${file}`);
+      request.post(
+        {
+          url: `https://${team}.slack.com/api/emoji.add`,
+          headers: {
+            Cookie: cookie,
+          },
+          formData: {
+            token,
+            name,
+            mode: 'data',
+            image,
+            _x_reason: 'customize-emoji-add',
+            _x_mode: 'online',
+          },
         },
-        host: `${team}.slack.com`,
-        method: 'POST',
-        path: '/api/emoji.add',
-        port: 443,
-      },
-      (res) => {
-        res.on('end', () => {
-          resolve();
-        });
-      }
-    );
-
-    req.write(body);
-    req.end();
-  });
-
-const putEmoji = async (name, file) =>
-  new Promise((resolve) => {
-    const image = fs.createReadStream(`../${file}`);
-    request.post(
-      {
-        url: `https://${team}.slack.com/api/emoji.add`,
-        headers: {
-          Cookie: cookie,
-        },
-        formData: {
-          token,
-          name,
-          mode: 'data',
-          image,
-          _x_reason: 'customize-emoji-add',
-          _x_mode: 'online',
-        },
-      },
-      (err, _, body) => {
-        const b = JSON.parse(body);
-        if (b && b.error) {
-          switch (b.error) {
-            case 'error_name_taken':
-            case 'error_name_taken_i18n':
-              console.log('add to skip');
-              skip.push(name);
-              break;
+        (err, _, body) => {
+          const b = JSON.parse(body);
+          if (b && b.error) {
+            switch (b.error) {
+              case 'error_name_taken':
+              case 'error_name_taken_i18n':
+                console.log('add to skip');
+                skip.push(name);
+                break;
+            }
+            console.log(b);
           }
-          console.log(b);
+          resolve();
         }
-        resolve();
-      }
-    );
-  });
-
-getEmoji()
-  .then((e) => {
-    console.log(`got ${e.length} existing emoji`);
-    e.forEach(({ name }) => {
-      delete emoji[name];
+      );
     });
-    console.log(`${Object.entries(emoji).length} new emoji`);
-  })
-  .then(async () => {
-    const entries = Object.entries(emoji);
-    const count = entries.length;
-    await entries.reduce(
-      (prev, [name, meta], i) =>
-        prev.then(async () => {
-          console.log(`[${i} of ${count}] putting ${name} - ${meta.file}`);
-          await putEmoji(name, meta.file);
-          await sleep(2500);
-        }),
-      Promise.resolve()
-    );
-  })
-  .then(async () => {
-    console.log('write new skip file');
-    fs.writeFileSync('skip.json', JSON.stringify(skip, null, 2));
-  });
+
+  getEmoji()
+    .then((e) => {
+      console.log(`got ${e.length} existing emoji`);
+      e.forEach(({ name }) => {
+        delete emoji[name];
+      });
+      console.log(`${Object.entries(emoji).length} new emoji`);
+    })
+    .then(async () => {
+      const entries = Object.entries(emoji);
+      const count = entries.length;
+      await entries.reduce(
+        (prev, [name, meta], i) =>
+          prev.then(async () => {
+            console.log(`[${i} of ${count}] putting ${name} - ${meta.file}`);
+            await putEmoji(name, meta.file);
+            await sleep(2500);
+          }),
+        Promise.resolve()
+      );
+    })
+    .then(async () => {
+      console.log('write new skip file');
+      fs.writeFileSync('skip.json', JSON.stringify(skip, null, 2));
+    });
+})();
